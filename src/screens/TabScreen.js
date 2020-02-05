70a33090666cc0a4a76d820c1eec7d6794ea685d
Brandon-Vasquez <brandon.vasquez96@gmail.com>
2020-02-05 11:47:04 -0500

    Initial commit

---
+++ src/screens/TabScreen.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, Tab, Tabs, Left, Body, Right, Title } from 'native-base';
+import Tab1 from './Tabs/tab1.js';
+import Tab2 from './Tabs/tab2.js';
+import Tab3 from './Tabs/tab3.js';
+export default class TabsExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header style={{backgroundColor: '#66b266' }}hasTabs>
+        <Left/>
+          <Body>
+            <Title style={{color:'#fff'}}>News App</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Tabs tabBarUnderlineStyle={{backgroundColor: '#66b266'}}>
+          <Tab tabStyle={{backgroundColor: '#99cc99'}} 
+          activeTabStyle={{backgroundColor: '#e5f2e5'}}
+          textStyle={{color: '#fff'}} 
+          activeTextStyle={{color: '#66b266'}} heading="Tab1">
+            <Tab1 />
+          </Tab>
+          <Tab tabStyle={{backgroundColor: '#99cc99'}} 
+          activeTabStyle={{backgroundColor: '#e5f2e5'}} 
+          textStyle={{color: '#fff'}} 
+          activeTextStyle={{color: '#66b266'}} heading="Tab2">
+            <Tab2 />
+          </Tab>
+          <Tab tabStyle={{backgroundColor: '#99cc99'}} 
+          activeTabStyle={{backgroundColor: '#e5f2e5'}} 
+          textStyle={{color: '#fff'}} 
+          activeTextStyle={{color: '#66b266'}} heading="Tab3">
+            <Tab3 />
+          </Tab>
+        </Tabs>
+      </Container>
+    );
+  }
+}
