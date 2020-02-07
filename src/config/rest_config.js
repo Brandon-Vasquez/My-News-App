51169320baf201cfbe0a3870ba74c7fdc54c49b6
Brandon-Vasquez <brandon.vasquez96@gmail.com>
2020-02-07 15:59:25 -0500

    NYTimes API Implementation

--- src/config/rest_config.js
+++ src/config/rest_config.js
@@ -1,2 +1,2 @@
-export const articles_url = 'https://api.nytimes.com/svc/topstories/v2'
+export const articles_url = 'https://api.nytimes.com/svc/topstories/v2/world.json'
 export const _api_key = 'ZcgIJtwEkNe2atSsNOiLTWXOx9h1v9UN'
--- src/service/news.js
+++ src/service/news.js
@@ -2,17 +2,21 @@ import { articles_url, _api_key, category, country_code} from '../config/rest_co
 
 export async function getArticles() {
   try {
-    let articles = await fetch(`${articles_url}?country=${country_code}&
-    category=${category}`, {
-      headers: {
-        'X-API-KEY': _api_key
-      }
-    })
+    // let articles = await fetch(`${articles_url}?country=${country_code}&
+    // category=${category}`, {
+    //   headers: {
+    //     'X-API-KEY': _api_key
+    //   }
+    // })
 
+    let articles = await fetch(`${articles_url}?api-key=${_api_key}`)
+    
+    // console.log(articles)
     let result = await articles.json()
     articles = null
+    console.log(result.results[0])
 
-    return result.articles
+    return result.results
   }
   catch(error) {
     throw error
